51627c44f1d602da2b2c2755d5118119d987ba82
Jhon Quitian <quitian.j2010@gmail.com>
2025-09-01 20:43:59 -0500

    feat: add documentation swagger to create a loan

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterRest.java
@@ -1,6 +1,15 @@
 package co.com.pragma.api;
 
+import co.com.pragma.api.dto.CreateLoanDTO;
 import co.com.pragma.api.exception.GlobalExceptionFilter;
+import co.com.pragma.model.loan.Loan;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -11,6 +20,37 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 
 @Configuration
 public class RouterRest {
+
+
+    @RouterOperation(
+            operation = @Operation(
+                    operationId = "loans", summary = "Save a user", tags = { "Loans" },
+                    requestBody = @RequestBody(
+                            required = true,
+                            description = "Create a Loan",
+                            content = @Content(
+                                    schema = @Schema(implementation = CreateLoanDTO.class),
+                                    examples = {
+                                            @ExampleObject(
+                                                    name = "Loan example",
+                                                    value = """
+                                                        {
+                                                            "amount": 5000,
+                                                            "term": 5,
+                                                            "dniUser": "123456789",
+                                                            "idLoanType": 1
+                                                        }
+                                                        """
+                                            )
+                                    }
+                            )
+                    ),
+                    responses = {
+                            @ApiResponse(responseCode = "201", description = "User created", content = @Content(schema = @Schema(implementation = Loan.class))),
+                            @ApiResponse(responseCode = "400", description = "Invalid data"),
+                    }
+            )
+    )
     @Bean
     public RouterFunction<ServerResponse> routerFunction(Handler handler, GlobalExceptionFilter filter) {
         return route(POST("/api/v1/requests"), handler::createOne)
